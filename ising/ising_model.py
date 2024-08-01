ba9dbc19a24858a51e59bbc180e22c88dbfe8872
stratisg <g.stratis@northeastern.edu>
2024-08-01 13:43:30 -0400

    Wrote a class for simulation. Improved code in Ising model.

--- ising/ising_model.py
+++ ising/ising_model.py
@@ -20,18 +20,19 @@ class IsingModel:
         self.radius = radius
         self.boundary_conds = boundary_conds
         self.l_neighborhood = [[] for _ in range(self.n_sites)]
+        self.energy_args = {}
 
         # TODO: Generalize for arbitrary geometries.
         # Note: The current function works only for rectangular lattices
         # and their generalizations.
-        self.spins = rnd.default_rng(seed).choice([0, 1], size=self.lattice)
+        self.spins = rnd.default_rng(seed).choice([-1, 1], size=self.lattice)
 
     def update(self, spin):
         """
         Flip the spin in a certain site. We assume that the spins can be 
-        either 0 or 1. 
+        either -1 or 1. 
         """
-        return (spin + 1) % 2
+        return -spin
 
     def generate_site_indices(self):
         """
@@ -73,19 +74,24 @@ class IsingModel:
         """
         Calculate the enerrgy corresponding to a given spin configuration 
         for the Ising model. The only sites that interact are those within 
-        a ball defined by the keyword argument radius.
+        a ball defined by the keyword argument radius. We calculate the 
+        average energy per site to avoid issues as we change the 
+        system's size.
         """
         energy_interaction = 0.0
         external_field_energy = 0.0
         for i_site, site_center in enumerate(self.site_indices):
-            energy_interaction_before = energy_interaction
             # Interaction energy.
             for neighbor_ in self.l_neighborhood[i_site]:
                 spin_ = self.spins[neighbor_]
                 # Check if the neighbor spin is aligned with the spin 
                 # at the site_center.
-                check_align = (spin_ + self.spins[tuple(site_center)] + 1) % 2
-                energy_interaction += check_align
+                # check_align = (spin_ + self.spins[tuple(site_center)] + 1) % 2
+                # energy_interaction += check_align
+                energy_interaction += self.check_align(
+                    spin_,self.spins[tuple(site_center)]
+                    )
+
             # Energy due to interaction with external field.
             external_field_energy += np.dot(self.external_field,
                                            self.spins[tuple(site_center)])
@@ -97,13 +103,51 @@ class IsingModel:
         # Multiiply by interaction.
         energy_interaction *= -self. interaction
 
-        return energy_interaction + external_field_energy
+        return (energy_interaction + external_field_energy) / self.n_sites
+    
+    def get_spins(self):
+        """
+        Copy the spin at each site so we can use the copied object for 
+        calculations.
+        """
+        spins_ = self.spins.copy()
 
+        return spins_
+
+    def check_align(self, spin0, spin1):
+        """
+        Check alignment beween two spins.
+        """
+        
+        return  np.abs((spin0 + spin1) / 2)
+    
+    def calculate_magnetization(self):
+        """
+        Calculate the system's average magnetization.
+        """
+        spins_ = self.get_spins() 
+
+        return spins_.sum() / self.n_sites
+    
+    def calculate_absolute_magnetization(self):
+        """
+        Calculate the absolute value of the system's magnetization.
+        """
+        magnetization = self.calculate_magnetization()
+
+        return np.abs(magnetization) / self.n_sites
 
 if __name__ == "__main__":
     lattice = np.array([4, 4], dtype=int)
     ising = IsingModel(lattice)
+    print("ising.spins")
+    print(ising.spins)
     ising.generate_site_indices()
     ising.get_neighborhood()
     energy = ising.calculate_energy()
     print(f"energy {energy}")
+    magnetization = ising.calculate_magnetization()
+    print(f"magnetization {magnetization}")
+    abs_magn = ising.calculate_absolute_magnetization()
+    print(f"absolute magnetization {abs_magn}")
+
---
+++ ising/simulation.py
@@ -0,0 +1,137 @@
+import numpy as np
+import numpy.random as rnd
+from ising_model import IsingModel
+
+
+# TODO: Should I convert simulation into a class?
+class Simulation:
+    """
+    The class that desctibes each simulation.
+    """
+    def __init__(self, ModelClass, model_args, n_samples, temperature,
+                 k_boltzmann=1, warmup_iter=int(1e3), seed_warmup=1821,
+                 seed_generation=1917) :
+        self.model = ModelClass(**model_args)
+        self.n_samples = n_samples
+        self.k_boltzmann = k_boltzmann
+        self.temperature = temperature
+        self.warmup_iter = warmup_iter
+        self.seed_generation = seed_generation
+        self.seed_warmup = seed_warmup
+        self.decorr_length = self.model.n_sites
+        self.energy = np.zeros(self.n_samples)
+        self.magnetization = np.zeros_like(self.energy)
+        self.magnetization_absolute = np.zeros_like(self.energy)
+    
+    def calculate_averages(self):
+        """
+        Calculate the averages of a given quantity.
+        """
+        return (self.energy.mean(axis=0), self.magnetization.mean(axis=0),
+                self.magnetization_absolute.mean(axis=0)
+                )
+    
+    # TODO: Save results. Name <Model>_<Model_Args>_<Quantity>
+    
+    def generate_samples(self):
+        """
+        Generate samples using Metropolis algorithm.
+        """    
+        # Generate the indices corresponding to each site
+        self.model.generate_site_indices()
+
+        # Warmup stage of the Metropolis aplgorithm.
+        self.metropolis_core(self.warmup_iter, self.seed_warmup,
+                             warmup_stage=True)
+        
+        # Using Metropolis for Sample generation.
+        self.metropolis_core(n_samples, self.seed_generation)
+
+    def metropolis_core(self, n_iterations, seed, warmup_stage=False):
+        """
+        The core part of the Metropolis algorithm.
+        """
+        accepted_attempts = 0
+        rng = rnd.default_rng(seed)
+        n_total_iters = n_iterations * self.decorr_length
+        for i_iter in range(n_total_iters):
+            # Select a site at random to change its spin.
+            site_proposed = tuple(
+                self.model.site_indices[rng.choice(self.model.n_sites)]
+            )
+            
+            # Store the current value of the spin at the proposed site.
+            spin_current = self.model.spins[site_proposed].copy()
+            
+            # Store energy of the current spin configuration. 
+            energy_current = self.model.calculate_energy(
+                **self.model.energy_args
+                )
+
+            # Updated spin at proposed site.
+            self.model.spins[spin_current] = self.model.update(
+                self.model.spins[spin_current]
+                )
+
+            # Calculate the energy of the updated spin configuration.
+            energy_updated = self.model.calculate_energy(
+                **self.model.energy_args
+                )
+            
+            # Acceptance condition
+            energy_diff = energy_updated - energy_current
+            if energy_diff <= 0:
+                accepted = 1
+            else:
+                prob  = np.exp(-energy_diff / (self.k_boltzmann *
+                                            self.temperature)
+                                )
+                # TODO: Consider using fixed seeds.
+                prob_test = rnd.default_rng().uniform()
+                accepted = prob > prob_test
+            # update acceptance rate
+            accepted_attempts += accepted
+            
+            # If the proposed spin update is not accepted, return the spin
+            # configuration to its previous state.
+            if not accepted:
+                self.model.spins[site_proposed] = spin_current.copy()
+            
+            # If we are not in the warmup stage, we output the
+            # generated samples.
+            if not i_iter % self.decorr_length and not warmup_stage:
+                self.energy[i_iter] = self.model.calculate_energy()
+                self.magnetization[i_iter] = (
+                    self.model.calculate_magnetization()
+                )
+                self.magnetization_absolute[i_iter] = (
+                    self.model.calculate_absolute_magnetization()
+                )
+
+        # Metropolis decorrelation length and acceptance ratio.
+        decorr_length = int(n_total_iters / accepted_attempts)
+        if warmup_stage:
+            self.decorr_length = int(max(decorr_length, 1))
+        accept_ratio = accepted_attempts / n_total_iters
+        print(f"Acceptance ratio: {accept_ratio:3f}.")
+        print(f"Number of de-correlation samples: {self.decorr_length}.")
+
+
+
+ising_args = dict(lattice=np.array([4, 4], dtype=int), interaction=1,
+                  external_field=0, radius=1,
+                  boundary_conds="periodic", seed=1959)
+
+# Simulation configuration.
+n_samples = int(1e4)
+temperature = 1e-6
+k_boltzmann = 1
+warmup_iter = int(1e3)
+simulation = Simulation(IsingModel, ising_args, n_samples, temperature,
+                        k_boltzmann, warmup_iter, seed_warmup=1821,
+                        seed_generation=1917)
+simulation.generate_samples()
+energy_avg, magn_avg, magn_abs_avg = simulation.calculate_averages()
+print(f"energy_avg: {energy_avg}")
+print(f"magnetization avg: {magn_avg}")
+print(f"magnetization absolute avg: {magn_abs_avg}")
