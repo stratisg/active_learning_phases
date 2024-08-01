Fix the simulation. Change definition of average absolute magnetization.

ising/ising_model.py
@@ -84,14 +84,10 @@ class IsingModel:
             # Interaction energy.
             for neighbor_ in self.l_neighborhood[i_site]:
                 spin_ = self.spins[neighbor_]
-                # Check if the neighbor spin is aligned with the spin 
-                # at the site_center.
-                # check_align = (spin_ + self.spins[tuple(site_center)] + 1) % 2
-                # energy_interaction += check_align
                 energy_interaction += self.check_align(
-                    spin_,self.spins[tuple(site_center)]
+                    spin_, self.spins[tuple(site_center)]
                     )
-
+                
             # Energy due to interaction with external field.
             external_field_energy += np.dot(self.external_field,
                                            self.spins[tuple(site_center)])
@@ -133,9 +129,9 @@ class IsingModel:
         """
         Calculate the absolute value of the system's magnetization.
         """
-        magnetization = self.calculate_magnetization()
+        magnetization_avg = self.calculate_magnetization()
 
-        return np.abs(magnetization) / self.n_sites
+        return np.abs(magnetization_avg)
 
 if __name__ == "__main__":
     lattice = np.array([4, 4], dtype=int)

ising/simulation.py
@@ -8,10 +8,9 @@ class Simulation:
     """
     The class that desctibes each simulation.
     """
-    def __init__(self, ModelClass, model_args, n_samples, temperature,
-                 k_boltzmann=1, warmup_iter=int(1e3), seed_warmup=1821,
-                 seed_generation=1917) :
-        self.model = ModelClass(**model_args)
+    def __init__(self, model, n_samples, temperature, k_boltzmann=1,
+                 warmup_iter=int(1e3), seed_warmup=1821, seed_generation=1917):
+        self.model = model
         self.n_samples = n_samples
         self.k_boltzmann = k_boltzmann
         self.temperature = temperature
@@ -36,16 +35,13 @@ class Simulation:
     def generate_samples(self):
         """
         Generate samples using Metropolis algorithm.
-        """    
-        # Generate the indices corresponding to each site
-        self.model.generate_site_indices()
-
+        """
         # Warmup stage of the Metropolis aplgorithm.
         self.metropolis_core(self.warmup_iter, self.seed_warmup,
                              warmup_stage=True)
         
         # Using Metropolis for Sample generation.
-        self.metropolis_core(n_samples, self.seed_generation)
+        self.metropolis_core(self.n_samples, self.seed_generation)
 
     def metropolis_core(self, n_iterations, seed, warmup_stage=False):
         """
@@ -62,24 +58,22 @@ class Simulation:
             
             # Store the current value of the spin at the proposed site.
             spin_current = self.model.spins[site_proposed].copy()
-            
+
             # Store energy of the current spin configuration. 
-            energy_current = self.model.calculate_energy(
-                **self.model.energy_args
-                )
-
+            energy_current = self.model.calculate_energy()
+            
             # Updated spin at proposed site.
-            self.model.spins[spin_current] = self.model.update(
-                self.model.spins[spin_current]
+            self.model.spins[site_proposed] = self.model.update(
+                self.model.spins[site_proposed]
                 )
-
+            
             # Calculate the energy of the updated spin configuration.
-            energy_updated = self.model.calculate_energy(
-                **self.model.energy_args
-                )
+            energy_updated = self.model.calculate_energy()
             
             # Acceptance condition
             energy_diff = energy_updated - energy_current
+            # print(f"energy_diff {energy_diff}")
+            # quit()
             if energy_diff <= 0:
                 accepted = 1
             else:
@@ -99,12 +93,14 @@ class Simulation:
             
             # If we are not in the warmup stage, we output the
             # generated samples.
-            if not i_iter % self.decorr_length and not warmup_stage:
-                self.energy[i_iter] = self.model.calculate_energy()
-                self.magnetization[i_iter] = (
+            
+            if not (i_iter % self.decorr_length) and not warmup_stage:
+                i_sample = i_iter // self.decorr_length
+                self.energy[i_sample] = self.model.calculate_energy()
+                self.magnetization[i_sample] = (
                     self.model.calculate_magnetization()
                 )
-                self.magnetization_absolute[i_iter] = (
+                self.magnetization_absolute[i_sample] = (
                     self.model.calculate_absolute_magnetization()
                 )
 
@@ -113,25 +109,38 @@ class Simulation:
         if warmup_stage:
             self.decorr_length = int(max(decorr_length, 1))
         accept_ratio = accepted_attempts / n_total_iters
+        print(39 * "=")
         print(f"Acceptance ratio: {accept_ratio:3f}.")
         print(f"Number of de-correlation samples: {self.decorr_length}.")
+        print(39 * "=")
 
 
+def optimize():
+    """
+    Function for recommending parameter values to use in the subsequent
+    simulation.  
+    """
 
-ising_args = dict(lattice=np.array([4, 4], dtype=int), interaction=1,
-                  external_field=0, radius=1,
-                  boundary_conds="periodic", seed=1959)
+if __name__ == "__main__":
+    ising_args = dict(lattice=np.array([4, 4], dtype=int), interaction=1,
+                    external_field=0, radius=1,
+                    boundary_conds="periodic", seed=1959)
+    ising_model = IsingModel(**ising_args)
+    ising_model.generate_site_indices()
+    ising_model.get_neighborhood()
 
-# Simulation configuration.
-n_samples = int(1e4)
-temperature = 1e-6
-k_boltzmann = 1
-warmup_iter = int(1e3)
-simulation = Simulation(IsingModel, ising_args, n_samples, temperature,
-                        k_boltzmann, warmup_iter, seed_warmup=1821,
-                        seed_generation=1917)
-simulation.generate_samples()
-energy_avg, magn_avg, magn_abs_avg = simulation.calculate_averages()
-print(f"energy_avg: {energy_avg}")
-print(f"magnetization avg: {magn_avg}")
-print(f"magnetization absolute avg: {magn_abs_avg}")
+    # Simulation configuration.
+    n_samples = int(1e3)
+    k_boltzmann = 1
+    warmup_iter = int(1e3)
+    temperature = 1
+
+
+    simulation = Simulation(ising_model, n_samples, temperature,
+                            k_boltzmann, warmup_iter, seed_warmup=1821,
+                            seed_generation=1917)
+    simulation.generate_samples()
+    energy_avg, magn_avg, magn_abs_avg = simulation.calculate_averages()
+    print(f"energy_avg: {energy_avg}")
+    print(f"magnetization avg: {magn_avg}")
+    print(f"magnetization absolute avg: {magn_abs_avg}")