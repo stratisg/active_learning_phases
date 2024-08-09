5bc8f1a48487579effa7722a71816f7133b1c9ad
stratisg <g.stratis@northeastern.edu>
2024-08-09 17:25:45 -0400

    Fixed the bound implementation.

--- ising/optimize.py
+++ ising/optimize.py
@@ -10,22 +10,26 @@ def optimize(model, n_input, step=1e-2, grad_threshold=1e4, n_iter=int(1e3),
     """
     # TODO: Find region where Hessian is zero.
     # Randomly initialize an input vector.
-    vec_in = torch.rand(n_input)
+    bounds = torch.tensor(bounds, dtype=torch.float)
+    vec_in = torch.rand(n_input) * (bounds[1] - bounds[0]) + bounds[0]
+
     for i_iter in range(n_iter):
-        print(f"i_iter {i_iter}")
         vec_in.requires_grad = True
         grad = torch.autograd.grad(model(vec_in), vec_in)[0]
         if torch.norm(grad) > grad_threshold:
             break
         vec_in.requires_grad = False
         vec_in += grad * step
-
         # Check if the updated vector is within the bounds. If it is not
         #  it within bounds, it projects it within the bounded region.
         for i_val, value in enumerate(vec_in):
             if value < bounds[0][i_val]:
-                vec_in = torch.rand(n_input)
+                print("Below low bound")
+                vec_in = (torch.rand(n_input) *\
+                          (bounds[1] - bounds[0]) + bounds[0])
             elif value > bounds[1][i_val]:
-                vec_in = torch.rand(n_input)
+                print("Above high bound")
+                vec_in = (torch.rand(n_input) *\
+                           (bounds[1] - bounds[0]) + bounds[0])
 
     return vec_in
