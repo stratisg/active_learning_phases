ca117aff43d21fe2e962d61612410680bb03dd71
Yorgos Stratis <g.stratis@northeastern.edu>
2024-08-12 10:59:52 -0400

    Include making plot of staggered magnetization. Fixed visualization script.

--- ising/visualization.py
+++ ising/visualization.py
@@ -40,7 +40,7 @@ def plot_quantity(quant_name, d_plot, figname, dpi=600, pics_dir="../pics",
 
 
 if __name__ == "__main__":
-    quant_name = "avg_magnetization"
-    l_values = load_data(results_dir_model, quant_name)
-    d_plot = dict(l_values=l_values, xlabel="J", ylabel="T")
-    plot_quantity(quant_name, d_plot, figname, dpi, pics_dir_model)
+    for quant_name in ["avg_magnetization", "avg_stagg_magnetization"]
+        l_values = load_data(results_dir_model, quant_name)
+        d_plot = dict(l_values=l_values, xlabel="J", ylabel="T")
+        plot_quantity(quant_name, d_plot, figname, dpi, pics_dir_model)
